a0aefdb127249f8fc04fccd3ae0c5c808118716a
Collin Edwards <collinedwards1579@gmail.com>
2018-01-24 19:39:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ return cart
 
 function viewCart() {
   var phrase = "In your cart, you have "
+  var ob = Object.keys
+  
+  // single + double cart
   
   if(cart.length === 0){
     return console.log("Your shopping cart is empty.")
   }
   
   else if(cart.length === 1){
-    return (phrase + `${cart[0]} at $${cart[0][Object.keys(cart[0])]}.` )
+    phrase += `${cart[0]} at $${cart[0][ob(cart[0])]}.` )
   }
   else if(cart.length === 2){
-    return (phrase + `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.` )
+    phrase += `${ob(cart[0])} at $${cart[0][ob(cart[0])]} and ${ob(cart[1])} at $${cart[1][ob(cart[1])]}.` )
   }
   else{
     for(var n = 0; n < cart.length; n++){
@@ -34,7 +37,7 @@ function viewCart() {
        phrase2 = ", and " 
      }
      else if(n === cart.length - 1){
-       phrase2 = ""
+       phrase2 = ", "
      }
      else{
        phrase2 = ""
